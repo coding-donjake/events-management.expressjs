5a15437f2c669baf0be76553938938315e982d64
coding-donjake <108913937+coding-donjake@users.noreply.github.com>
2023-11-13 22:00:24 +0800

    update

--- src/controllers/order/update.ts
+++ src/controllers/order/update.ts
@@ -19,6 +19,22 @@ const orderUpdateController = async (req: Request, res: Response) => {
           content: req.body.order,
         },
       });
+      await prisma.orderSupply.deleteMany({
+        where: { orderId: req.body.order.id },
+      });
+      for (let i = 0; i < req.body.orderSupply.length; i++) {
+        const element = req.body.orderSupply[i];
+        element.orderId = req.body.order.id;
+        const orderSupply = await prisma.orderSupply.create({ data: element });
+        await prisma.orderSupplyLog.create({
+          data: {
+            type: "create",
+            orderSupplyId: orderSupply.id,
+            operatorId: req.body.decodedToken.id,
+            content: orderSupply,
+          },
+        });
+      }
       if (req.body.order.status !== "arrived") return res.status(200).send();
     }
     for (let i = 0; i < req.body.orderSupply.length; i++) {
