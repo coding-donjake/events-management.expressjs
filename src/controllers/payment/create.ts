b818e88a3cb73b7b10ee096ea6dc876a75b63964
coding-donjake <108913937+coding-donjake@users.noreply.github.com>
2024-01-02 14:54:39 +0800

    update

--- src/controllers/payment/create.ts
+++ src/controllers/payment/create.ts
@@ -1,10 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
+import eventSelectConstant from "../../constants/event/select";
 
 const prisma: PrismaClient = new PrismaClient();
 
 const paymentCreateController = async (req: Request, res: Response) => {
   try {
+    let event = await prisma.event.findUnique({
+      where: { id: req.body.event.id },
+      select: eventSelectConstant,
+    });
+    if ((event as any)!.balance - req.body.payment.amount < 0) {
+      res.status(501).json();
+      return;
+    }
     const payment = await prisma.payment.create({ data: req.body.payment });
     await prisma.paymentLog.create({
       data: {
@@ -15,7 +24,7 @@ const paymentCreateController = async (req: Request, res: Response) => {
       },
     });
     await prisma.event.update({
-      where: { id: payment.eventId! },
+      where: { id: req.body.event.id! },
       data: {
         balance: { decrement: payment.amount },
       },
